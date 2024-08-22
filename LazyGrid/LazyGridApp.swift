dbbba0b6f5b7c6de58bef6b6e87328892026ac02
Bosco Ho <boscojwho@gmail.com>
2024-08-22 16:02:03 -0700

    Initial Commit

---
+++ LazyGrid/LazyGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyGridApp.swift
+//  LazyGrid
+//
+//  Created by Bosco Ho on 2024-08-22.
+//
+
+import SwiftUI
+
+@main
+struct LazyGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
